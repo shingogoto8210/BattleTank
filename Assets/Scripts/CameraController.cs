ef494cb16f36853144f56c095bfa07703cc32845
shingogoto8210 <chupps8210@gmail.com>
2021-05-10 22:28:26 +0900

    15 複数のカメラを切り替えるまで終了

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Camera FPSCamera;
+    private bool mainCameraON = true;
+
+    [SerializeField] private AudioListener mainListener;
+    [SerializeField] private AudioListener FPSListener;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera.enabled = true;
+        FPSCamera.enabled = false;
+
+        mainListener.enabled = true;
+        FPSListener.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C)&& mainCameraON == true)
+        {
+            mainCamera.enabled = false;
+            FPSCamera.enabled = true;
+            mainCameraON = false;
+
+            mainListener.enabled = false;
+            FPSListener.enabled = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.C) && mainCameraON == false)
+        {
+            mainCamera.enabled = true;
+            FPSCamera.enabled = false;
+            mainCameraON = true;
+
+            mainListener.enabled = true;
+            FPSListener.enabled = false;
+        }
+    }
+}
