d8cdff0e2d02bb31bf2dadf22a73fc3d80ef80bd
shingogoto8210 <chupps8210@gmail.com>
2021-05-15 11:29:04 +0900

    19連射の発射数に制限を加える

--- Assets/Scripts/ShotShell.cs
+++ Assets/Scripts/ShotShell.cs
@@ -7,26 +7,30 @@ public class ShotShell : MonoBehaviour
     public float shotSpeed;
     [SerializeField] private GameObject shellPrefab;
     [SerializeField] private AudioClip shotSound;
-    public bool canshot;
-    public float timer;
+    private bool canshot;
+    private float timer;
+    public int shotCount;
 
     void Update()
     {
         timer += Time.deltaTime;
-        if (timer > 1.0f)
+        if (timer > 0.3f)
         {
             canshot = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space)&& canshot == true)
+        if (Input.GetKeyDown(KeyCode.Space)&& canshot == true && shotCount > 0)
         {
             GameObject shell = Instantiate(shellPrefab, transform.position, Quaternion.identity);
             Rigidbody shellRb = shell.GetComponent<Rigidbody>();
             shellRb.AddForce(transform.forward * shotSpeed);
             Destroy(shell, 3.0f);
             AudioSource.PlayClipAtPoint(shotSound, transform.position);
+            shotCount -= 1;
             timer = 0;
             canshot = false;
         }
+
+
     }
 }
