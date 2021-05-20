7bd0873ad6b644b1ebbd5d7c507814be336e1a2c
shingogoto8210 <chupps8210@gmail.com>
2021-05-20 22:10:41 +0900

    30転んだタンクをもとに戻す　完了

---
+++ Assets/Scripts/Recover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recover : MonoBehaviour
+{
+    private Vector3 rot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rot = transform.eulerAngles;
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            transform.eulerAngles = new Vector3(0, rot.y, 0);
+        }
+    }
+}
